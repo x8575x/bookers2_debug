4678d4ced3f8df0bec610d1d7f7dd95163ff6349
EC2 Default User <ec2-user@ip-172-31-22-189.ec2.internal>
2022-04-25 04:08:31 +0000

    [Add]フォーロー・フォロワー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,35 @@ class User < ApplicationRecord
   validates :introduction, length: { maximum: 50}
 
 
-
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
 
+
+
+
+  
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+  
+  # フォローしたときの処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
+  
 end
